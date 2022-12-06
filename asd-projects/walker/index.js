93394fe3a6680eee694a5fd339eb3f43be2eba82
Tyler Serigne <tylerserigne@gmail.com>
2022-12-06 19:34:09 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,13 +10,30 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+
+  var KEY = {
+    "LEFT": 37,
+    "RIGHT": 39,
+    "UP": 38,
+    "DOWN": 40,
+  }
+
+  var positionX = 0;
+  var positionY = 0;
+  var speedX = 0;
+  var speedY = 0;
   
+
   // Game Item Objects
 
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);
+  $(document).on('keyup', handleKeyUp);
+                             // change 'eventType' to the type of event you want to handle
+
+
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,16 +44,36 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    redrawGameItem();
+    repositionGameItem();
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+
+  function handleKeyDown(event) {
+      if (event.which === KEY.LEFT){
+        console.log("LEFT Pressed")
+          speedX = -5;
+      }
+
+      if (event.which === KEY.RIGHT){
+        console.log("RIGHT Pressed")
+          speedX = 5;
+    }
+      if (event.which === KEY.UP){
+        console.log("UP Pressed")
+        speedY = -5;
+
+    }
+      if (event.which === KEY.DOWN){
+        console.log("DOWN Pressed")
+          speedY = 5;
+    };
 
   }
+
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
@@ -51,4 +88,16 @@ function runProgram(){
     $(document).off();
   }
   
+  function repositionGameItem(){
+    positionX += speedX;
+    positionY += speedY;
+  };
+  
+  function redrawGameItem(){
+    $("#walker").css("left",positionX);
+    $("#walker").css("top",positionY);
+  };
+
+
 }
+
